d2c4dfdb0e98fe2f681fadac5883ac87e71fe945
chiidzzz <69384519+chiidzzz@users.noreply.github.com>
2023-05-20 18:23:33 +0300

    Add files via upload
    added functionality of maxTQ

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
       selectedAircraft: "",
@@ -20,18 +20,18 @@ Vue.createApp({
         L1216: 5756,
       },
       basicWeight: "",
-      fuel: "",
+      fuel: 1400,
       fuelErrorMessage: "",
-      load: "",
+      load: 600,
       loadUnit: "lbs",
       takeoffGW: 0.0,
-      qnh: "",
-      qnhUnit: "hPa",
-      fat: "",
+      qnh: 29.92,
+      qnhUnit: "inHg",
+      fat: 20,
       indicatedAltitude: "",
-      pressureAltitude: 0,
-      densityAltitude: 0,
-      maxTQ: 0.0,
+      pressureAltitude: "",
+      densityAltitude: "",
+      maxTQ: null,
       predictedHoverTQ2ft: 0.0,
       predictedHoverTQ4ft: 0.0,
       predictedHoverTQ30ft: 0.0,
@@ -40,7 +40,6 @@ Vue.createApp({
   },
   watch: {
     selectedAircraft() {
-      this.updateBasicWeight();
       this.calculateTakeoffGW();
     },
     fuel() {
@@ -64,10 +63,12 @@ Vue.createApp({
     fat() {
       this.calculatePressureAltitude();
       this.calculateDensityAltitude();
+      this.calculateMaxTQ();
     },
     indicatedAltitude() {
       this.calculatePressureAltitude();
       this.calculateDensityAltitude();
+      this.calculateMaxTQ();
     },
   },
   methods: {
@@ -90,6 +91,13 @@ Vue.createApp({
 
       this.takeoffGW = (basicWeightLbs + loadLbs + fuelLbs).toFixed(0);
     },
+    limitIndicatedAltitude(event) {
+      if (this.indicatedAltitude < 0) {
+        this.indicatedAltitude = 0;
+      } else if (this.indicatedAltitude > 14000) {
+        this.indicatedAltitude = 14000;
+      }
+    },
     calculatePressureAltitude() {
       const indicatedAltitudeValue = parseFloat(this.indicatedAltitude);
       const qnhValue = parseFloat(this.qnh);
@@ -104,16 +112,125 @@ Vue.createApp({
       const pressureAltitude = parseFloat(this.pressureAltitude);
       const temperature = parseFloat(this.fat);
 
-      const densityAltitude = pressureAltitude + 120 * (temperature - 15);
+      const densityAltitude =
+        pressureAltitude +
+        120 * (temperature - (15 - pressureAltitude * 0.00198));
 
       this.densityAltitude = isNaN(densityAltitude)
         ? 0
         : densityAltitude.toFixed(0);
     },
-  },
-  computed: {
-    densityAltitude() {
-      // Calculation logic for density altitude
+
+    Fat0() {
+      const B23 = parseFloat(this.fat);
+      const result = 0.0095 * Math.pow(B23, 2) - 2.1571 * B23 + 166.05;
+      return result > 100 ? 100 : result;
+    },
+    Fat2000() {
+      const B20 = parseFloat(this.fat);
+      const result =
+        -0.00074786 * Math.pow(B20, 3) +
+        0.10096154 * Math.pow(B20, 2) -
+        5.72457265 * B20 +
+        204.30769231;
+      return result > 100 ? 100 : result;
+    },
+    Fat4000() {
+      const B17 = parseFloat(this.fat);
+      const result =
+        -0.00017721 * Math.pow(B17, 3) +
+        0.01506658 * Math.pow(B17, 2) -
+        1.47211474 * B17 +
+        128.53679353;
+      return result > 100 ? 100 : result;
+    },
+    Fat6000() {
+      const B14 = parseFloat(this.fat);
+      const result =
+        0.0000101 * Math.pow(B14, 3) -
+        0.00277056 * Math.pow(B14, 2) -
+        0.81259019 * B14 +
+        112.78571429;
+      return result > 100 ? 100 : result;
+    },
+    Fat8000() {
+      const B11 = parseFloat(this.fat);
+      const result =
+        -0.00005439 * Math.pow(B11, 3) +
+        0.00251748 * Math.pow(B11, 2) -
+        0.83150738 * B11 +
+        104.1;
+      return result > 100 ? 100 : result;
+    },
+    Fat10000() {
+      const B8 = parseFloat(this.fat);
+      const result =
+        0.00000668 * Math.pow(B8, 4) -
+        0.00041017 * Math.pow(B8, 3) -
+        0.00270513 * Math.pow(B8, 2) -
+        0.39245176 * B8 +
+        93.94326262;
+      return result > 100 ? 100 : result;
+    },
+    Fat12000() {
+      const B5 = parseFloat(this.fat);
+      const result =
+        0.00000867 * Math.pow(B5, 4) -
+        0.0005301 * Math.pow(B5, 3) -
+        0.00275058 * Math.pow(B5, 2) -
+        0.29468669 * B5 +
+        86.69107363;
+      return result > 100 ? 100 : result;
+    },
+    Fat14000() {
+      const B2 = parseFloat(this.fat);
+      const result =
+        0.00000629 * Math.pow(B2, 4) -
+        0.00037949 * Math.pow(B2, 3) -
+        0.00326224 * Math.pow(B2, 2) -
+        0.31544789 * B2 +
+        80.16723277;
+      return result > 100 ? 100 : result;
+    },
+    calculateMaxTQ() {
+      const N3 = parseFloat(this.pressureAltitude);
+      if ((N3 === 0 || N3 > 0) && N3 < 2000) {
+        this.maxTQ = this.Fat0() - (this.Fat0() - this.Fat2000()) * (N3 / 2000);
+      } else if ((N3 === 2000 || N3 > 2000) && N3 < 4000) {
+        this.maxTQ =
+          this.Fat2000() -
+          ((this.Fat2000() - this.Fat4000()) * (N3 - 2000)) / 2000;
+      } else if ((N3 === 4000 || N3 > 4000) && N3 < 6000) {
+        this.maxTQ =
+          this.Fat4000() -
+          ((this.Fat4000() - this.Fat6000()) * (N3 - 4000)) / 2000;
+      } else if ((N3 === 6000 || N3 > 6000) && N3 < 8000) {
+        this.maxTQ =
+          this.Fat6000() -
+          ((this.Fat6000() - this.Fat8000()) * (N3 - 6000)) / 2000;
+      } else if ((N3 === 8000 || N3 > 8000) && N3 < 10000) {
+        this.maxTQ =
+          this.Fat8000() -
+          ((this.Fat8000() - this.Fat10000()) * (N3 - 8000)) / 2000;
+      } else if ((N3 === 10000 || N3 > 10000) && N3 < 12000) {
+        this.maxTQ =
+          this.Fat10000() -
+          ((this.Fat10000() - this.Fat12000()) * (N3 - 10000)) / 2000;
+      } else if ((N3 === 12000 || N3 > 12000) && N3 < 14000) {
+        this.maxTQ =
+          this.Fat12000() -
+          ((this.Fat12000() - this.Fat14000()) * (N3 - 12000)) / 2000;
+      } else if (N3 === 14000) {
+        this.maxTQ = this.Fat14000();
+      } else {
+        this.maxTQ = null;
+      }
+      if (typeof this.maxTQ === "number") {
+        this.maxTQ = this.maxTQ.toFixed(1); // Convert maxTQ to a string with one decimal place
+      } else {
+        alert("Check Indicated Altitude between 0 and 14000!");
+      }
     },
   },
+  computed: {},
 }).mount("#app");
